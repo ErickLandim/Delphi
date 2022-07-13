77af3917668e5d21240d975008b68c545e4d1b07
ErickLandim <103335925+ErickLandim@users.noreply.github.com>
2022-07-13 17:41:53 -0300

    inicio

---
+++ Mini_Project_Banco/Pessoa.pas
@@ -0,0 +1,18 @@
+unit Pessoa;
+
+interface
+
+type
+  TPessoa = class
+
+  private
+  public
+    id: integer;
+    Nome: string;
+    Tipo: string;
+    Email: String;
+  end;
+
+implementation
+
+end.
